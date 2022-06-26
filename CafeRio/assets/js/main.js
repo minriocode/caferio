8c180a037423b311291afb77a62c58d3e6030d85
mincodeopt <mincodeopt@gmail.com>
2022-06-26 17:17:27 +0600

    caferio 3.0 uploaded

--- CafeRio/assets/js/main.js
+++ CafeRio/assets/js/main.js
@@ -25,32 +25,6 @@
             }
         });
 
-       /*  // homepage slider
-        $(".homepage-slider").owlCarousel({
-            items: 1,
-            loop: true,
-            autoplay: true,
-            nav: true,
-            dots: false,
-            navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
-            responsive:{
-                0:{
-                    items:1,
-                    nav:false,
-                    loop:true
-                },
-                600:{
-                    items:1,
-                    nav:true,
-                    loop:true
-                },
-                1000:{
-                    items:1,
-                    nav:true,
-                    loop:true
-                }
-            }
-        }); */
 
         // logo carousel
         $(".logo-carousel-inner").owlCarousel({
@@ -186,5 +160,11 @@
     Fancybox
     -------------------------------*/
     $('[data-fancybox="gallery"]').fancybox();
-
+	
+	/*------------------------------
+    SlickNav
+    -------------------------------*/
+	$('#top-menu').slicknav({
+		label: '',
+	});
 }(jQuery));
